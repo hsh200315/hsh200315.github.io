14e7b45d5a778d2f57393e0e562f06e4d1812497
hsh <hsh200315@naver.com>
2023-11-10 16:34:18 +0900

    feat: 초기 설정 및 스킬 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
 import config from '../gitprofile.config';
-import GitProfile from './components/GitProfile';
+import GitProfile from '@arifszn/gitprofile';
+import '@arifszn/gitprofile/dist/style.css';
 
 function App() {
-  return <GitProfile config={config} />;
+  return (
+    <GitProfile
+      config={{
+        github: {
+          username: 'hsh200315',
+          sortBy: 'stars',
+          limit: 8,
+          exclude: { forks: false },
+        },
+        skills: ['html', 'css', 'JavaScript', 'React.js', 'C'],
+        education: [
+          {
+            institution: 'Major: Computer Science and Engineering',
+            degree: 'Sungkyunkwan University',
+            from: '2020',
+            to: 'Present',
+          },
+          {
+            degree: 'Inje High School',
+            from: '2017',
+            to: '2019',
+          },
+        ],
+      }}
+    />
+  );
 }
 
 export default App;
