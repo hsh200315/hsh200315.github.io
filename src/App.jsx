ea66c2df0f575066013b395012882e9877260e42
hsh <hsh200315@naver.com>
2023-11-10 19:16:24 +0900

    feat: 1차 완료

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,62 @@ function App() {
         github: {
           username: 'hsh200315',
           sortBy: 'stars',
-          limit: 8,
+          limit: 12,
           exclude: { forks: false },
         },
+        social: {
+          linkedin: '',
+          twitter: '',
+          mastodon: '',
+          facebook: '',
+          instagram: '',
+          youtube: '',
+          dribbble: '',
+          behance: '',
+          medium: '',
+          dev: '',
+          stackoverflow: '',
+          skype: '',
+          telegram: '',
+          website: '',
+          phone: '',
+          email: 'hsh200315@g.skku.edu',
+        },
         skills: ['html', 'css', 'JavaScript', 'React.js', 'C'],
+        experiences: [
+          {
+            position: 'President of SCAR',
+            from: '2023. 09',
+            to: 'Present',
+          },
+          {
+            position: 'SSA',
+            company: 'Team SOSD',
+            from: '2023. 09',
+            to: 'Present',
+          },
+          {
+            position: 'SPARK: POP-IT',
+            from: '2023. 09',
+            to: 'Present',
+          },
+          {
+            position: 'Summer Industry-Academic Cooperation Project',
+            from: '2023. 07',
+            to: '2023. 09',
+          },
+        ],
         education: [
+          {
+            degree: 'Introduction to Artificial Intelligence',
+            from: '2023.09',
+            to: 'Present',
+          },
+          {
+            degree: 'Open Source Software Practice',
+            from: '2023.09',
+            to: 'Present',
+          },
           {
             institution: 'Major: Computer Science and Engineering',
             degree: 'Sungkyunkwan University',
@@ -24,6 +75,12 @@ function App() {
             degree: 'Inje High School',
             from: '2017',
             to: '2019',
+          },
+        ],
+        certifications: [
+          {
+            name: 'SKKUTHON Grand Prize',
+            year: '2023. 09',
           },
         ],
       }}
